b9b707a92e569a9050eec73a84520f901bf9f59b
drinkincode] <jonnysmith696910@gmail.com>
2024-08-21 19:11:39 -0500

    collision detection

--- actors/player.py
+++ actors/player.py
@@ -12,46 +12,39 @@ class Player(Entity):
         self.did_collide = False
         
         self.enabled_direction_dict = {'up': True, 'down': True, 'left': True, 'right': True}
-        self.current_direction = 'up'
         
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def update(self):
-        current_direction = self.current_direction
-        if self.check_collision():
-            self.enabled_direction_dict[self.current_direction] = False
         self.movement()
-        self.enabled_direction_dict[current_direction] = True
         
     def movement(self):
         self.set_movement_controls('w', 's', 'a', 'd')
-        
                  
     def set_movement_controls(self, up, down, left, right):
         if held_keys[up] and self.enabled_direction_dict['up']:
-            self.current_direction = 'up'
-            self.y += time.dt * self.speed
+            if not self.check_collision(Vec3(0, time.dt * self.speed, 0)):
+                self.y += time.dt * self.speed
             return
         elif held_keys[down] and self.enabled_direction_dict['down']:
-            self.current_direction = 'down'
-            self.y -= time.dt * self.speed
+            if not self.check_collision(Vec3(0, -time.dt * self.speed, 0)):
+                self.y -= time.dt * self.speed
             return
         elif held_keys[left] and self.enabled_direction_dict['left']:
-            self.current_direction = 'left'
-            self.x -= time.dt * self.speed
+            if not self.check_collision(Vec3(-time.dt * self.speed, 0, 0)):
+                self.x -= time.dt * self.speed
             return
         elif held_keys[right] and self.enabled_direction_dict['right']:
-            self.current_direction = 'right'
-            self.x += time.dt * self.speed
+            if not self.check_collision(Vec3(time.dt * self.speed, 0, 0)):
+                self.x += time.dt * self.speed
             return
 
-    def check_collision(self):
+    def check_collision(self, offset):
+        original_position = self.position
+        self.position += offset
         hit_info = self.intersects()
-        if hit_info.hit:
-            print(f'Collision with {hit_info.entity}')
-            print(hit_info.world_point)
-            print(hit_info.world_point)
-            return True
+        self.position = original_position
+        return hit_info.hit
 
 
