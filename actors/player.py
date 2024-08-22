fa80905c172376fbaa1f7c747994b777ac9d3125
drinkincode] <jonnysmith696910@gmail.com>
2024-08-21 19:04:24 -0500

    collision?

--- actors/player.py
+++ actors/player.py
@@ -9,25 +9,49 @@ class Player(Entity):
         self.scale_x = 2.33
         self.collider = 'box'
         self.speed = 5
-
+        self.did_collide = False
+        
+        self.enabled_direction_dict = {'up': True, 'down': True, 'left': True, 'right': True}
+        self.current_direction = 'up'
+        
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def update(self):
+        current_direction = self.current_direction
+        if self.check_collision():
+            self.enabled_direction_dict[self.current_direction] = False
         self.movement()
-
+        self.enabled_direction_dict[current_direction] = True
+        
     def movement(self):
         self.set_movement_controls('w', 's', 'a', 'd')
         
                  
     def set_movement_controls(self, up, down, left, right):
-        if held_keys[up]:
+        if held_keys[up] and self.enabled_direction_dict['up']:
+            self.current_direction = 'up'
             self.y += time.dt * self.speed
-        if held_keys[down]:
+            return
+        elif held_keys[down] and self.enabled_direction_dict['down']:
+            self.current_direction = 'down'
             self.y -= time.dt * self.speed
-        if held_keys[left]:
+            return
+        elif held_keys[left] and self.enabled_direction_dict['left']:
+            self.current_direction = 'left'
             self.x -= time.dt * self.speed
-        if held_keys[right]:
+            return
+        elif held_keys[right] and self.enabled_direction_dict['right']:
+            self.current_direction = 'right'
             self.x += time.dt * self.speed
+            return
+
+    def check_collision(self):
+        hit_info = self.intersects()
+        if hit_info.hit:
+            print(f'Collision with {hit_info.entity}')
+            print(hit_info.world_point)
+            print(hit_info.world_point)
+            return True
 
 
