da1b736426962f56977b53b7b6e638bb2d3b897f
drinkincode] <jonnysmith696910@gmail.com>
2024-08-21 20:24:14 -0500

    stat bar added to ui

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from ursina import *
 from ursina.prefabs.platformer_controller_2d import PlatformerController2d
 from maps.map import Room, Map
 from actors.player import Player
+
+from menus.stat_menu import StatBar
 # window.vsync = False
 window.borderless = False
 app = Ursina()
@@ -19,6 +21,8 @@ camera.fov = 20
 
 # Boarder Walls
 map_1 = Map()
+stat_bar = StatBar(stat_name='health', color=color.green, position=(-0.5, 0.35))
+
 
 # Add interactive elements
 player = Player(position=(-3, -2, 0))
