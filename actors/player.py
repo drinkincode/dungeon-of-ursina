b63eaa3ba9e0abe40261d8d510be70bed3bb9d95
drinkincode] <jonnysmith696910@gmail.com>
2024-08-21 19:57:32 -0500

    added stat handler to player

--- actors/player.py
+++ actors/player.py
@@ -1,5 +1,6 @@
 from ursina import *
-
+from stats.baseStat import BaseStat
+from stats.baseStatHandler import BaseStatHandler
 class Player(Entity):
     def __init__(self, **kwargs):
         super().__init__()
@@ -9,36 +10,37 @@ class Player(Entity):
         self.scale_x = 2
         self.collider = 'box'
         self.speed = 5
-        self.did_collide = False
-        
+        self.jump_height = 0.3
+        self.gravity = 1
+        self.vertical_velocity = 0
+        self.grounded = False
         self.enabled_direction_dict = {'up': True, 'down': True, 'left': True, 'right': True}
+        self.init_stat_list = [
+            ['health', 100],
+            ['stamina', 100],
+            ['mana', 100]
+        ] 
+        self.stat_handler = BaseStatHandler(self.init_stat_list)
         
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def update(self):
         self.movement()
-            
+
     def movement(self):
-        self.set_movement_controls('w', 's', 'a', 'd')
-                 
-    def set_movement_controls(self, up, down, left, right):
-        if held_keys[up] and self.enabled_direction_dict['up']:
+        if held_keys['w'] and self.enabled_direction_dict['up']:
             if not self.check_collision(Vec3(0, time.dt * self.speed, 0)):
                 self.y += time.dt * self.speed
-            return
-        elif held_keys[down] and self.enabled_direction_dict['down']:
+        if held_keys['s'] and self.enabled_direction_dict['down']:
             if not self.check_collision(Vec3(0, -time.dt * self.speed, 0)):
                 self.y -= time.dt * self.speed
-            return
-        elif held_keys[left] and self.enabled_direction_dict['left']:
+        if held_keys['a'] and self.enabled_direction_dict['left']:
             if not self.check_collision(Vec3(-time.dt * self.speed, 0, 0)):
                 self.x -= time.dt * self.speed
-            return
-        elif held_keys[right] and self.enabled_direction_dict['right']:
+        if held_keys['d'] and self.enabled_direction_dict['right']:
             if not self.check_collision(Vec3(time.dt * self.speed, 0, 0)):
                 self.x += time.dt * self.speed
-            return
 
     def check_collision(self, offset):
         original_position = self.position
@@ -46,5 +48,3 @@ class Player(Entity):
         hit_info = self.intersects()
         self.position = original_position
         return hit_info.hit
-
-
